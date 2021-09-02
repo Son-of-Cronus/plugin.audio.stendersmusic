c2ab10a3be5c690ff2e4d31dd73be7e2941dd5b2
Hades <79419919+Son-of-Cronus@users.noreply.github.com>
2021-09-02 19:04:54 +0200

    Update xbmcutil.py
    Committed changes to line 10 for python 3

--- resources/lib/xbmcutil.py
+++ resources/lib/xbmcutil.py
@@ -7,7 +7,8 @@ def addMenuItem(strName, strUrl, bIsPlayable='true', icon=None, fanart=None):
     li = xbmcgui.ListItem(strName)
     if not icon is None :
         iconPath = icon
-        li = xbmcgui.ListItem(label=strName, iconImage=iconPath, thumbnailImage=iconPath)
+        li = xbmcgui.ListItem(label=strName)
+	li.setArt({'iconImage':'iconPath', 'thumbnailImage':'iconPath'})
     else :
         li = xbmcgui.ListItem(strName)
     if not fanart is None :
