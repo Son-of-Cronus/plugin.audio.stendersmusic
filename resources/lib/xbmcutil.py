eba97bc33d0cdf182cf682e4a45b33705f8aae61
Hades <79419919+Son-of-Cronus@users.noreply.github.com>
2021-09-03 09:26:36 +0200

    Update xbmcutil.py
    Committed reformat due to indentation error in python 3

--- resources/lib/xbmcutil.py
+++ resources/lib/xbmcutil.py
@@ -1,24 +1,40 @@
+# -*- coding: utf-8 -*-
+
 import sys
 
 from kodi_six import xbmcplugin, xbmcgui
-	
-def addMenuItem(strName, strUrl, bIsPlayable='true', icon=None, fanart=None):
+
+
+def addMenuItem(
+    strName,
+    strUrl,
+    bIsPlayable='true',
+    icon=None,
+    fanart=None,
+    ):
     li = xbmcgui.ListItem(strName)
-    if not icon is None :
+    if not icon is None:
         iconPath = icon
-        li = xbmcgui.ListItem(label = strName)
-	li.setArt({'iconImage' : 'iconPath', 'thumbnailImage' : 'iconPath'})
-    else :
+        li = xbmcgui.ListItem(label=strName)
+        li.setArt({'iconImage': 'iconPath', 'thumbnailImage': 'iconPath'
+                  })
+    else:
         li = xbmcgui.ListItem(strName)
-    if not fanart is None :
+    if not fanart is None:
         fanartimg = fanart
-        li.setProperty('fanart_image',fanartimg)
-    li.setProperty('IsPlayable', bIsPlayable)
-    li.addContextMenuItems([ ('Vernieuwen...', 'Container.Refresh') ])
+        li.setProperty('fanart_image', fanartimg)
+        li.setProperty('IsPlayable', bIsPlayable)
+        li.addContextMenuItems([('Vernieuwen...', 'Container.Refresh')])
     if bIsPlayable == 'true':
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=strUrl, listitem=li)
-    else :
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=sys.argv[0]+'?browse='+strUrl, listitem=li, isFolder=True)
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=strUrl,
+                                    listitem=li)
+    else:
+        xbmcplugin.addDirectoryItem(handle=addon_handle,
+                                    url=sys.argv[0] + '?browse='
+                                    + strUrl, listitem=li,
+                                    isFolder=True)
+
 
 def endOfList():
     xbmcplugin.endOfDirectory(addon_handle)
+
